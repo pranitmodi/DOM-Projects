fc4d10314de8171b60a0fd80d73b54f4c23e3066
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2023-09-23 12:33:16 +0530

    done

--- custom-cursor/script.js
+++ custom-cursor/script.js
@@ -1,6 +1,7 @@
 var main = document.querySelector("#main")
 var crsr = document.querySelector(".cursor")
 
+//mouseenter and mouseleave - also can be used for a particular part
 main.addEventListener("mousemove",function(okay)
 {
     console.log(okay.x + " " + okay.y)
